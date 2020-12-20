343c5db8cb703547d12be5998751a87a617fcdd7
Andrés Felipe Rincón Gamboa <andres.rincon@mail.mcgill.ca>
2020-12-19 19:48:36 -0500

    Update KafkaProducerConfig.java

--- KafkaProducerConfig.java
+++ KafkaProducerConfig.java
@@ -3,7 +3,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-public class Main {
+public class KafkaProducerConfig {
   
     Properties props = new Properties();
     props.put("bootstrap.servers", "localhost:9092");
